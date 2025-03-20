4616ebedd396dda8d2adedddf188d17198602cda
son.tran22 <son.trannam.bku@gmail.com>
2025-03-20 13:04:08 +0700

    Adding new features - Adding record in database, change attribute of device - Using rsjx to implement sse for transmiting data to frontend - Adding sending notification to frontend when a feed exceeds maxValue - Adding authentication - Adding endpoint to add new device

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [JwtModule.register({
+    secret: process.env.JWT_SERCRET_KEY,
+    global: true,
+    signOptions: {expiresIn: '2 days'}
+  })],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
+ 
--- src/mqtt/mqtt.service.ts
+++ src/mqtt/mqtt.service.ts
@@ -1,54 +1,89 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { ForbiddenException, Injectable, InternalServerErrorException, NotFoundException, OnModuleDestroy } from '@nestjs/common';
 import * as mqtt from 'mqtt';
+import { Subject } from 'rxjs';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class MqttService implements OnModuleDestroy {
-  private client: mqtt.MqttClient;
-  
-  constructor() {
-    this.client = mqtt.connect('mqtts://io.adafruit.com', {
-      port: Number(process.env.MQTT_PORT),
-      username: process.env.MQTT_USERNAME, 
-      password: process.env.MQTT_PASSWORD, 
-    });
+	private client: mqtt.MqttClient;
+	private eventSubject = new Subject<any>();
 
-    this.client.on('connect', () => {
-      console.log('✅ Connected to Adafruit IO MQTT');
-    });
+	constructor(private readonly prisma: PrismaService
+	) {
+		this.client = mqtt.connect('mqtts://io.adafruit.com', {
+			port: Number(process.env.MQTT_PORT),
+			username: process.env.MQTT_USERNAME,
+			password: process.env.MQTT_PASSWORD,
+		});
 
-    this.client.on('error', (err) => {
-      console.error('❌ MQTT Error:', err);
-    });
+		this.client.on('connect', () => {
+			console.log('Connected to Adafruit IO MQTT');
+		});
 
-    this.client.on('message', (topic, message) => {
-      const data = message.toString();
-      console.log(`📥 Received from ${topic}: ${data}`);
-    });
-  }
+		this.client.on('error', (err) => {
+			console.error('MQTT Error:', err);
+		});
 
-  async sendDataToAdafruit(feed: string, value: string) {
-    const topic = `${process.env.ADAFRUIT_TOPIC}/${feed}`;
-    this.client.publish(topic, value, (err) => {
-      if (err) {
-        console.error('❌ Error publishing to MQTT:', err);
-      } else {
-        console.log(`🚀 Published to ${topic}: ${value}`);
-      }
-    });
-  }
+		// Listen to device
+		this.client.on('message', async (topic, message) => {
+			const data = Number(message.toString());
+			const device = await this.getDevice(topic)
+			// Save record to database
+			try {
+				const record = await this.prisma.record.create({
+					data: {
+						value: data,
+						dateCreated: new Date(),
+						device: {
+							connect: { ID: device.ID }
+						}
+					}
+				})
+			} catch (err) {
+				console.error(err);
+				throw new InternalServerErrorException("An error occurred! Please try again.");
+			}
+			if (device.maxValue && device.maxValue < data) {
+				this.eventSubject.next({ device });
+			} 			
+		});
+	}
 
-  // private subscribeToDeviceData(feed: string) {
-  //   const topic = `${process.env.ADAFRUIT_TOPIC}/${feed}`;
-  //   this.client.subscribe(topic, (err) => {
-  //     if (err) {
-  //       console.error('❌ Error subscribing:', err);
-  //     } else {
-  //       console.log(`📌 Subscribed to ${topic}`);
-  //     }
-  //   });
-  // }
+	async sendDataToAdafruit(topic: string, value: number) {
+		this.client.publish(topic, `${value}`, (err) => {
+			if (err) {
+				console.error('Error publishing to MQTT:', err);
+			} else {
+				console.log(`Published to ${topic}: ${value}`);
+			}
+		});
+	}
 
-  onModuleDestroy() {
-    this.client.end();
-  }
+	public subscribeToDeviceData(topic: string) {
+		this.client.subscribe(topic, (err) => {
+			if (err) {
+				console.error('Error subscribing:', err);
+			} else {
+				console.log(`Subscribed to ${topic}`);
+			}
+		});
+	}
+
+	onModuleDestroy() {
+		this.client.end();
+	}
+	
+	getEvents() {
+		return this.eventSubject.asObservable();
+	}
+
+	async getDevice(topic: string) {
+        const device = await this.prisma.device.findUnique({
+            where: { topic: topic }
+        });
+        if (!device) {
+            throw new NotFoundException("Topic doesn't exist");
+        }
+        return device
+    }
 }
